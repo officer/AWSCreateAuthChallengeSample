de9a2051ebc7e275cd496a305b90381d5b3d95b1
chocolatle <notbeafraidofanything@gmail.com>
2017-03-10 02:07:04 +0900

    Add project files.

---
+++ CreateAuthChallenge/app.js
@@ -0,0 +1,24 @@
+﻿console.log('Loading');
+
+exports.handler = function (event, context) {
+
+    if (event != null) {
+        console.log('event = ' + JSON.stringify(event));
+    }
+    else {
+        console.log('No event object');
+
+    }
+
+    if (event.request.challengeName == 'CUSTOM_CHALLENGE') {
+        event.response.publicChallengeParameters = {};
+        event.response.publicChallengeParameters.answer = 6;
+        event.response.privateChallengeParameters = {};
+        event.response.privateChallengeParameters.answer = 6;
+        event.response.challengeMetadata = 'SIMPLE_CHALLENGE';
+    }
+
+    console.log('event = ' + JSON.stringify(event));
+
+    context.done(null, event);  // SUCCESS with message
+};
